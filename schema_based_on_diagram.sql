1111a2dcf1d59ddcb0e0962ac1f911bf6345b4a1
Matt <mwbertolucci@gmail.com>
2023-05-10 16:02:24 -0400

    Add indexes to FK Keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,6 +5,8 @@ CREATE TABLE medical_histories(
   status VARCHAR(100) NOT NULL
 );
 
+CREATE INDEX idx_medical_histories_patient_id ON medical_histories(patient_id);
+
 CREATE TABLE treatments(
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   TYPE VARCHAR(100) NOT NULL,
@@ -16,6 +18,9 @@ CREATE TABLE treatments_history(
   medical_history_id INT NOT NULL REFERENCES medical_histories(id),
   treatment_id INT NOT NULL REFERENCES treatments(id)
 );
+
+CREATE INDEX idx_treatments_history_medical_history_id ON treatments_history(medical_history_id);
+CREATE INDEX idx_treatments_history_treatment_id ON treatments_history(treatment_id);
 
 CREATE TABLE patients(
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -33,11 +38,16 @@ CREATE TABLE invoices(
   medical_history_id INT NOT NULL REFERENCES medical_histories(id)
 );
 
-CREATE TABLE invoice_item(
+CREATE INDEX idx_invoices_medical_history_id ON invoices(medical_history_id);
+
+CREATE TABLE invoice_items(
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   unit_price DECIMAL NOT NULL,
   quantity INT NOT NULL,
   total_price DECIMAL NOT NULL,
   invoice_id INT NOT NULL REFERENCES invoices(id),
   treatment_id INT NOT NULL REFERENCES treatments(id)
-)
+);
+
+CREATE INDEX idx_invoice_items_invoice_id ON invoice_items(invoice_id);
+CREATE INDEX idx_invoice_items_treatment_id ON invoice_items(treatment_id);
